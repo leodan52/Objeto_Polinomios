d9cc5804d19f7d9bf2b533dd4c73dba7f946a6df
= <=>
2023-05-10 13:39:28 -0600

    Station

--- TOOLS/polinomial.py
+++ TOOLS/polinomial.py
@@ -1,363 +1,381 @@
-
 import numpy as np
+import scipy.integrate as integrate
+
 
 class Polinomio:
 
-	''' Define un objeto polinomio en forma de serie de potencias. Está definida la suma (+)
-la resta (-), el producto (*) y la composición (&)'''
+    """Define un objeto polinomio en forma de serie de potencias. Está definida la suma (+)
+    la resta (-), el producto (*) y la composición (&)"""
 
-	def __init__(self, lista, ordenDescendente = False):
+    def __init__(self, lista, ordenDescendente=False):
+        self.__vector = np.array(lista).astype(np.float64)
+        self.__grado = self.__vector.size - 1
+        self.__representacion = ""
+        self.__ordenDescendente = ordenDescendente
 
-		self.__vector = np.array(lista).astype(np.float64)
-		self.__grado = self.__vector.size - 1
-		self.__representacion = ""
-		self.__ordenDescendente = ordenDescendente
+        self.__toString()
 
-		self.__toString()
+    def __repr__(self):
+        return f"{self.__representacion}"
 
-	def __repr__(self):
-		return f'{self.__representacion}'
+    # ------------ Definición de las operaciones del objeto ---------------------------------
 
-	# ------------ Definición de las operaciones del objeto ---------------------------------
+    def __add__(self, other):
+        if not isinstance(other, Polinomio):
+            other = Polinomio.__Nuevo_polinomio([other])
 
-	def __add__(self, other):
+        p1, p2, l = Polinomio.__Igualar_len(self.__vector, other.__vector)
+        suma = p1 + p2
 
-		if not isinstance(other, Polinomio):
-			other = Polinomio.__Nuevo_polinomio([other])
+        return Polinomio.__Nuevo_polinomio(suma)
 
-		p1, p2, l = Polinomio.__Igualar_len(self.__vector, other.__vector)
-		suma = p1 + p2
+    def __radd__(self, other):
+        return Polinomio.__Sumar(self, other)
 
-		return Polinomio.__Nuevo_polinomio(suma)
+    def __sub__(self, other):
+        return Polinomio.__Restar(self, other)
 
-	def __radd__(self, other):
-		return Polinomio.__Sumar(self, other)
+    def __rsub__(self, other):
+        return Polinomio.__Restar(self, other)
 
-	def __sub__(self, other):
-		return Polinomio.__Restar(self, other)
+    def __neg__(self):
+        aux = self.__vector.copy()
+        aux = (-1) * aux
 
-	def __rsub__(self, other):
-		return Polinomio.__Restar(self, other)
+        return Polinomio.__Nuevo_polinomio(aux)
 
-	def __neg__(self):
-		aux = self.__vector.copy()
-		aux = (-1)*aux
+    def __pos__(self):
+        return self
 
-		return Polinomio.__Nuevo_polinomio(aux)
+    def __mul__(self, other):
+        if not isinstance(other, Polinomio):
+            other = Polinomio.__Nuevo_polinomio([other])
 
-	def __pos__(self):
-		return self
+        p1, p2, l = Polinomio.__Igualar_len(self.__vector, other.__vector)
+        Tensor = Polinomio.Tensor_producto(l, l)
+        salida = np.einsum("ijk,j,k -> i", Tensor, p1, p2)
 
-	def __mul__(self, other):
+        return Polinomio.__Nuevo_polinomio(salida)
 
-		if not isinstance(other, Polinomio):
-			other = Polinomio.__Nuevo_polinomio([other])
+    def __rmul__(self, other):
+        return Polinomio.__Multiplicar(self, other)
 
-		p1, p2, l = Polinomio.__Igualar_len(self.__vector, other.__vector)
-		Tensor = Polinomio.Tensor_producto(l,l)
-		salida = np.einsum('ijk,j,k -> i',Tensor,p1,p2)
+    def __pow__(self, n_):
+        if not isinstance(n_, int):
+            raise TypeError(f"El exponente es {type(n_)}. Debe ser int")
+        elif n_ < 0:
+            raise TypeError(f"El exponente es negativo. Aún no se define")
+        elif n_ == 0:
+            return Polinomio.One()
+        elif n_ == 1:
+            return self
 
-		return Polinomio.__Nuevo_polinomio(salida)
+        Tensor = Polinomio.Tensor_producto(self.__grado + 1, self.__grado + 1)
+        salida = Polinomio.__Nuevo_polinomio(self.__vector.copy())
+        m = 1
 
-	def __rmul__(self, other):
-		return Polinomio.__Multiplicar(self, other)
+        while m < n_:
+            salida = Polinomio.__Multiplicar(salida, self)
+            m += 1
 
-	def __pow__(self, n_):
-		if not isinstance(n_, int):
-			raise TypeError(f'El exponente es {type(n_)}. Debe ser int')
-		elif n_ < 0:
-			raise TypeError(f'El exponente es negativo. Aún no se define')
-		elif n_ == 0:
-			return Polinomio.One()
-		elif n_ == 1:
-			return self
+        return salida
 
-		Tensor = Polinomio.Tensor_producto(self.__grado + 1, self.__grado + 1)
-		salida = Polinomio.__Nuevo_polinomio(self.__vector.copy())
-		m = 1
+    def __and__(self, other):
+        m = 0
+        salida = 0
+        for a in self.__vector:
+            aux0 = Polinomio.__Potenciar(other, m)
+            aux = Polinomio.__Multiplicar(aux0, a)
+            salida = Polinomio.__Sumar(aux, salida)
 
-		while m < n_:
-			salida = Polinomio.__Multiplicar(salida, self)
-			m += 1
+            m += 1
 
-		return salida
+        return salida
 
-	def __and__(self, other):
-		m = 0
-		salida = 0
-		for a in self.__vector:
-			aux0 = Polinomio.__Potenciar(other, m)
-			aux = Polinomio.__Multiplicar(aux0, a)
-			salida = Polinomio.__Sumar(aux, salida)
+    def evaluar(self, x):
+        salida = 0
 
-			m += 1
+        for exponente in range(self.__grado + 1):
+            salida += self.__vector[exponente] * (x**exponente)
 
-		return salida
+        return salida
 
-	def evaluar(self, x):
-		salida = 0
+    # ----------------- Definición de métodos de extracción de atributos ------------------------------
 
-		for exponente in range(self.__grado + 1):
-			salida += self.__vector[exponente]*(x**exponente)
+    def __len__(self):
+        return self.__grado
 
-		return salida
+    def __getitem__(self, n):
+        try:
+            return self.__vector[n]
+        except IndexError:
+            return 0
+
+    def __setitem__(self, n, nuevo):
+        if not isinstance(nuevo, int) and not isinstance(nuevo, float):
+            raise TypeError(f"Debe ser un número real, no un {type(nuevo)}")
+        try:
+            self.__vector[n] = nuevo
+        except IndexError:
+            aux = np.zeros(n + 1)
+            self.__vector, aux, ll = Polinomio.__Igualar_len(self.__vector, aux)
+            self.__vector[n] = nuevo
 
+        while self.__vector[-1] == 0:
+            self.__vector = np.delete(self.__vector, -1)
 
-	#----------------- Definición de métodos de extracción de atributos ------------------------------
+        self.__toString()
 
-	def __len__(self):
-		return self.__grado
+    def copy(self):
+        return Polinomio.__Nuevo_polinomio(self.__vector.copy())
 
-	def __getitem__(self, n):
-		try:
-			return self.__vector[n]
-		except IndexError:
-			return 0
+    def OrdenarAscendente(self):
+        self.__ordenDescendente = False
 
+        self.__toString()
 
-	def __setitem__(self, n, nuevo):
-		if not isinstance(nuevo, int) and not isinstance(nuevo, float):
-			raise TypeError(f'Debe ser un número real, no un {type(nuevo)}')
-		try:
-			self.__vector[n] = nuevo
-		except IndexError:
-			aux = np.zeros(n+1)
-			self.__vector, aux, ll =  Polinomio.__Igualar_len(self.__vector, aux)
-			self.__vector[n] = nuevo
+    def OrdenarDescendente(self):
+        self.__ordenDescendente = True
 
-		while self.__vector[-1] == 0:
-			self.__vector = np.delete(self.__vector, -1)
+        self.__toString()
 
-		self.__toString()
+    def lista(self):
+        return [
+            coeficiente
+            for coeficiente in (
+                self.__vector if not self.__ordenDescendente else self.__vector[::-1]
+            )
+        ]
 
-	def copy(self):
-		return Polinomio.__Nuevo_polinomio(self.__vector.copy())
+    @property
+    def ordenDescendente(self):
+        return self.__ordenDescendente
 
-	def OrdenarAscendente(self):
+    @ordenDescendente.setter
+    def ordenDescendente(self, desendente):
+        self.__ordenDescendente = desendente
 
-		self.__ordenDescendente = False
+    # -------------------------------------------------------
 
-		self.__toString()
+    def __toString(self):
+        """Construye la representación del polinomio"""
 
-	def OrdenarDescendente(self):
+        entradas = Polinomio.__Num2strg(self.__vector)
+        salida = []
+        m = 0
 
-		self.__ordenDescendente = True
+        for termino in entradas:
+            if termino in ["+1", "-1"] and m != 0:
+                termino = termino.replace("1", "")
 
-		self.__toString()
+            if termino == "+0":
+                pass
+            elif m == 0:
+                salida.append(termino)
+            elif m == 1:
+                aux = f"{termino}x"
+                salida.append(aux)
+            else:
+                aux = f"{termino}x^{m}"
+                salida.append(aux)
+            m += 1
 
-	def lista(self):
-		return [coeficiente for coeficiente in(self.__vector if not self.__ordenDescendente else self.__vector[::-1])]
+        if self.__ordenDescendente:
+            salida = "".join(salida[::-1])
+        else:
+            salida = "".join(salida)
 
-	@property
-	def ordenDescendente(self):
-		return self.__ordenDescendente
+        if salida == "":
+            salida = "0"
+        elif salida[0] == "+":
+            salida = salida[1:]
 
-	@ordenDescendente.setter
-	def ordenDescendente(self, desendente):
-		self.__ordenDescendente = desendente
+        salida = salida.replace("+", " + ")
+        salida = salida.replace("-", " - ")
 
+        self.__representacion = salida.strip()
 
-	#-------------------------------------------------------
+    @classmethod
+    def __Nuevo_polinomio(cls, lista):
+        """Regresa un objeto del tipo Polinomio. Necesario para definir operaciones."""
 
-	def __toString(self):
+        return cls(lista)
 
-		''' Construye la representación del polinomio '''
+    @classmethod
+    def Zero(cls):
+        """Retorna el objeto Polinomio 0, el neutro aditivo"""
 
-		entradas = Polinomio.__Num2strg(self.__vector)
-		salida = []
-		m = 0
+        return cls([0])
 
-		for termino in entradas:
-			if termino in ["+1", "-1"] and m != 0:
-				termino = termino.replace("1", "")
+    @classmethod
+    def One(cls):
+        """Regresa el objeto Polinomio 1, la identidad, el neutro multiplcativo"""
 
-			if termino == "+0":
-				pass
-			elif m == 0:
-				salida.append(termino)
-			elif m == 1:
-				aux = f'{termino}x'
-				salida.append(aux)
-			else:
-				aux = f'{termino}x^{m}'
-				salida.append(aux)
-			m += 1
+        return cls([1.0])
 
-		if self.__ordenDescendente:
-			salida = "".join(salida[::-1])
-		else:
-			salida = "".join(salida)
+    @classmethod
+    def Identity_composicion(cls):
+        """Regresa como objeto Polinomio la identidad o neutro de la composición"""
 
-		if salida == "":
-			salida = "0"
-		elif salida[0] == "+":
-			salida = salida[1:]
+        return cls([0, 1.0])
 
-		salida = salida.replace("+", " + ")
-		salida = salida.replace("-", " - ")
+    # ---------- Metodos para realizar operaciones dentro del módulo --------------------------------
 
-		self.__representacion = salida.strip()
+    @staticmethod
+    def __Restar(a, b):
+        return a + (-b)
 
+    @staticmethod
+    def __Sumar(a, b):
+        return a + b
 
-	@classmethod
-	def __Nuevo_polinomio(cls, lista):
+    @staticmethod
+    def __Multiplicar(a, b):
+        return a * b
 
-		'''Regresa un objeto del tipo Polinomio. Necesario para definir operaciones. '''
+    @staticmethod
+    def __Potenciar(a, n):
+        return a**n
 
-		return cls(lista)
+    # ------------------- Métodos para la comparación -------------------------------------------
 
-	@classmethod
-	def Zero(cls):
+    def __eq__(self, other):
+        if not isinstance(other, Polinomio):
+            other = Polinomio.__Nuevo_polinomio([other])
 
-		'''Retorna el objeto Polinomio 0, el neutro aditivo '''
+        p1, p2, l = Polinomio.__Igualar_len(self.__vector, other.__vector)
 
-		return cls([0])
+        return all(p1 == p2)
 
-	@classmethod
-	def One(cls):
+    def __ne__(self, other):
+        return not (self == other)
 
-		'''Regresa el objeto Polinomio 1, la identidad, el neutro multiplcativo '''
+    # ------------------- Métodos estáticos del la clase --------------------------------------
 
-		return cls([1.0])
+    @staticmethod
+    def Conmu(a, b):
+        """Se define el conmutador de la composición"""
 
-	@classmethod
-	def Identity_composicion(cls):
+        return (a & b) - (b & a)
+
+    @staticmethod
+    def __Igualar_len(p1_, p2_):
+        """Iguala la longitud de los vectores numpy para las operaciones"""
+
+        p1 = p1_.copy()
+        p2 = p2_.copy()
 
-		''' Regresa como objeto Polinomio la identidad o neutro de la composición '''
-
-		return cls([0, 1.0])
-
-	# ---------- Metodos para realizar operaciones dentro del módulo --------------------------------
-
-	@staticmethod
-	def __Restar(a, b):
-		return a + (-b)
-
-	@staticmethod
-	def __Sumar(a, b):
-		return a + b
-
-	@staticmethod
-	def __Multiplicar(a, b):
-		return a*b
-
-	@staticmethod
-	def __Potenciar(a, n):
-		return a**n
-
-	# ------------------- Métodos para la comparación -------------------------------------------
-
-	def __eq__(self, other):
-
-		if not isinstance(other, Polinomio):
-			other = Polinomio.__Nuevo_polinomio([other])
-
-		p1, p2, l = Polinomio.__Igualar_len(self.__vector, other.__vector)
-
-
-		return all(p1 == p2)
-
-	def __ne__(self, other):
-		return not (self == other)
-
-
-	# ------------------- Métodos estáticos del la clase --------------------------------------
-
-	@staticmethod
-	def Conmu(a,b):
-
-		'''Se define el conmutador de la composición '''
-
-		return (a&b)-(b&a)
-
-	@staticmethod
-	def __Igualar_len(p1_, p2_):
-
-		''' Iguala la longitud de los vectores numpy para las operaciones '''
-
-		p1 = p1_.copy()
-		p2 = p2_.copy()
-
-		l1 = len(p1)
-		l2 = len(p2)
-
-		while l1 != l2:
-			if l1 > l2:
-				p2 = np.append(p2, 0)
-				l2 += 1
-			elif l1 < l2:
-				p1 = np.append(p1, 0)
-				l1 += 1
-
-		return p1, p2, len(p1)
-
-
-	@staticmethod
-	def __Num2strg(Array):
-
-		'''Convierte los datos numéricos en strings. Herramienta para la representación '''
-
-		salida = []
-		for i in Array:
-			delta = abs(i - int(i))
-
-			if i >= 0:
-				signo = "+"
-			else:
-				signo = ""
-
-			if delta == 0.0:
-				salida.append(signo + str(int(i)))
-			else:
-				salida.append(signo + str(i))
-		return salida
-
-	@staticmethod
-	def Tensor_producto(jj,kk):
-
-		''' Contruye el tensor de jj x kk que define el producto polinomial '''
-
-		tensor = np.zeros((jj + kk -1, jj, kk))
-
-		for i in range(jj + kk -1):
-			for j in range(jj):
-				for k in range(kk):
-					if i == j + k:
-						tensor[i,j,k] = 1
-		return tensor
-
-
-
-
-	@staticmethod
-	def CambioBase(poli, *base):
-		gradoPolinomio = len(poli)
-		gradoBase = 0
-
-		#Obtiene el grado de la base
-		for vector in base:	
-			if len(vector) > gradoBase:
-				gradoBase = len(vector)
-		
-
-		if gradoPolinomio > gradoBase:
-			raise ValueError("El grado de la base debe de ser igual o mayor que el del polinomio")
-
-
-		estandar2base = np.zeros((gradoBase+1, len(base)))
-		listaPolinomio = poli.lista() if not poli.ordenDescendente else poli.lista()[::-1]
-		candidato = np.zeros(gradoBase+1)
-		candidato[:len(listaPolinomio)] = listaPolinomio
-
-		for i in range(len(base)):
-			coef = base[i].lista()
-			# Si el vector está en orden descendente lo "endereza"
-			if(base[i].ordenDescendente):
-				coef = coef[::-1]
-
-			estandar2base[:len(coef), i] = coef
-
-		base2estandar = np.linalg.inv(estandar2base)
-		newCoef = np.matmul(base2estandar, candidato)
-
-		return newCoef.tolist()
+        l1 = len(p1)
+        l2 = len(p2)
+
+        while l1 != l2:
+            if l1 > l2:
+                p2 = np.append(p2, 0)
+                l2 += 1
+            elif l1 < l2:
+                p1 = np.append(p1, 0)
+                l1 += 1
+
+        return p1, p2, len(p1)
+
+    @staticmethod
+    def __Num2strg(Array):
+        """Convierte los datos numéricos en strings. Herramienta para la representación"""
+
+        salida = []
+        for i in Array:
+            delta = abs(i - int(i))
+
+            if i >= 0:
+                signo = "+"
+            else:
+                signo = ""
+
+            if delta == 0.0:
+                salida.append(signo + str(int(i)))
+            else:
+                salida.append(signo + str(i))
+        return salida
+
+    @staticmethod
+    def Tensor_producto(jj, kk):
+        """Contruye el tensor de jj x kk que define el producto polinomial"""
+
+        tensor = np.zeros((jj + kk - 1, jj, kk))
+
+        for i in range(jj + kk - 1):
+            for j in range(jj):
+                for k in range(kk):
+                    if i == j + k:
+                        tensor[i, j, k] = 1
+        return tensor
+
+    @staticmethod
+    def CambioBase(poli, *base):
+        gradoPolinomio = len(poli)
+        gradoBase = 0
+
+        # Obtiene el grado de la base
+        for vector in base:
+            if len(vector) > gradoBase:
+                gradoBase = len(vector)
+
+        if gradoPolinomio > gradoBase:
+            raise ValueError(
+                "El grado de la base debe de ser igual o mayor que el del polinomio"
+            )
+
+        estandar2base = np.zeros((gradoBase + 1, len(base)))
+        listaPolinomio = (
+            poli.lista() if not poli.ordenDescendente else poli.lista()[::-1]
+        )
+        candidato = np.zeros(gradoBase + 1)
+        candidato[: len(listaPolinomio)] = listaPolinomio
+
+        for i in range(len(base)):
+            coef = base[i].lista()
+            # Si el vector está en orden descendente lo "endereza"
+            if base[i].ordenDescendente:
+                coef = coef[::-1]
+
+            estandar2base[: len(coef), i] = coef
+
+        base2estandar = np.linalg.inv(estandar2base)
+        newCoef = np.matmul(base2estandar, candidato)
+
+        return newCoef.tolist()
+
+    @staticmethod
+    def ortogonalizar(
+        base,
+        normalizar=True,
+        productoInterno=lambda a, b: Polinomio.productoInternoIntegral(a, b),
+    ):
+        nuevaBase = (
+            [base[0] * (1 / productoInterno(base[0], base[0]))]
+            if normalizar
+            else [base[0]]
+        )
+        for i in range(1, len(base)):
+            candidato = base[i]
+            for j in range(i):
+                candidato -= Polinomio.__proj(nuevaBase[j], base[i])
+
+        return nuevaBase
+
+    @staticmethod
+    def productoInternoIntegral(
+        a, b, limiteInferior=-1, limiteSuperior=1, ponderacion=lambda x: 1
+    ):
+        polinomioInterno = a * b
+        resultado = integrate.quad(
+            lambda x: polinomioInterno.evaluar(x) * ponderacion(x),
+            limiteInferior,
+            limiteSuperior,
+        )
+
+        return resultado[0]
+
+    @staticmethod
+    def __proj(u, v, producto):
+        return v * (producto(v, u) / producto(u, u))
--- main.py
+++ main.py
@@ -2,99 +2,101 @@
 
 from TOOLS.polinomial import *
 
+
 def main():
+    f = [1, 1]
+    g = [1, -1]
+    h = [-3, 2]
+    I = [2, 1, -3]
 
-	f = [1,1]
-	g = [1, -1]
-	h = [-3, 2]
-	I = [2, 1, -3]
+    g_ = Polinomio(g)
+    h_ = Polinomio(h)
+    f_ = Polinomio(f)
+    I_ = Polinomio(I)
 
-	g_ = Polinomio(g)
-	h_ = Polinomio(h)
-	f_ = Polinomio(f)
-	I_ = Polinomio(I)
+    tupla = (f_, g_, h_, I_)
 
-	tupla = (f_, g_, h_, I_)
+    print("Sean los polinomios:")
+    print(f"f(x) = {f_}")
+    print(f"g(x) = {g_}")
+    print(f"h(x) = {h_}")
+    print(f"I(x) = {I_}")
+    print(f"parte del grupo de polinimos de grado n, P_n(x)")
 
-	print("Sean los polinomios:")
-	print(f'f(x) = {f_}')
-	print(f'g(x) = {g_}')
-	print(f'h(x) = {h_}')
-	print(f'I(x) = {I_}')
-	print(f'parte del grupo de polinimos de grado n, P_n(x)')
-
-	segundo = ''' Donde se definen las operaciones de suma (+), producto (*) y
+    segundo = """ Donde se definen las operaciones de suma (+), producto (*) y
 composición (&).
 
-Ejemplos:'''
-	print(segundo)
-	print("Suma de polinomios:\n")
+Ejemplos:"""
+    print(segundo)
+    print("Suma de polinomios:\n")
 
-	for i in tupla:
-		for j in tupla:
-			print(i, "+", j, "=", i + j)
-		print()
+    for i in tupla:
+        for j in tupla:
+            print(i, "+", j, "=", i + j)
+        print()
 
+    print("\nMultiplicación algebraica:\n")
 
-	print("\nMultiplicación algebraica:\n")
+    for i in tupla:
+        for j in tupla:
+            print(i, "*", j, "=", i * j)
+        print()
 
-	for i in tupla:
-		for j in tupla:
-			print(i, "*", j, "=", i * j)
-		print()
+    print("\nPotencia:\n")
 
-	print("\nPotencia:\n")
+    for poli in tupla:
+        for n in (2, 3):
+            print(poli, "^", n, " = ", poli**n, sep="")
+        print()
 
-	for poli in tupla:
-		for n in (2,3):
-			print(poli, "^", n, " = ", poli**n, sep="")
-		print()
+    print("\nComposición:\n")
 
-	print("\nComposición:\n")
+    for i in tupla:
+        for j in tupla:
+            print(i, "&", j, "=", i & j)
+        print()
 
-	for i in tupla:
-		for j in tupla:
-			print(i, "&", j, "=", i & j)
-		print()
+    print("\nCon su conmutador:\n")
 
-	print("\nCon su conmutador:\n")
+    for i in tupla:
+        for j in tupla:
+            print("[", i, ", ", j, "]", " = ", Polinomio.Conmu(i, j), sep="")
+        print()
 
-	for i in tupla:
-		for j in tupla:
-			print("[", i, ", ", j, "]", " = ", Polinomio.Conmu(i,j), sep="")
-		print()
+    print(Polinomio.Identity_composicion() & tupla[0])
+    print(Polinomio.Tensor_producto(2, 2))
 
-	print(Polinomio.Identity_composicion() & tupla[0])
-	print(Polinomio.Tensor_producto(2,2))
+    print("\n\n")
+    test = Polinomio([5, 4, 3])
+    base0 = Polinomio([1, 0, 1])
+    base1 = Polinomio([1, 0, -1])
+    base2 = Polinomio([0, 1, 2])
 
-	print("\n\n")
-	test = Polinomio([5,4,3])
-	base0 = Polinomio([1,0,1])
-	base1 = Polinomio([1,0,-1])
-	base2 = Polinomio([0,1,2])
+    print(f"Sea P = {test} un polinomio representado en la base estandar")
+    print(f"Y una base alternativa representada por los polinomios")
+    print(f"b1 = {base0}")
+    print(f"b2 = {base1}")
+    print(f"b3 = {base2}")
 
-	print(f'Sea P = {test} un polinomio representado en la base estandar')
-	print(f'Y una base alternativa representada por los polinomios')
-	print(f'b1 = {base0}')
-	print(f'b2 = {base1}')
-	print(f'b3 = {base2}')
+    coeficientes = Polinomio.CambioBase(test, base0, base1, base2)
 
-	coeficientes = Polinomio.CambioBase(test, base0, base1, base2)
-	
-	result = coeficientes[0]*base0 + coeficientes[1]*base1 + coeficientes[2]*base2
+    result = coeficientes[0] * base0 + coeficientes[1] * base1 + coeficientes[2] * base2
 
-	print(f'P puede ser representado en la base b de la siguiente manera')
-	print(f'P = {coeficientes[0]}*b1 + {coeficientes[1]}*b2 + {coeficientes[2]}*b3')
-	print("")
-	print(f'Si se realiza las operaciones se obtiene: P = {result}')
-	print("")
+    print(f"P puede ser representado en la base b de la siguiente manera")
+    print(f"P = {coeficientes[0]}*b1 + {coeficientes[1]}*b2 + {coeficientes[2]}*b3")
+    print("")
+    print(f"Si se realiza las operaciones se obtiene: P = {result}")
+    print("")
+    print("")
 
+    base1 = Polinomio([1])
+    base2 = Polinomio([0, 1])
+    base3 = Polinomio([0, 0, 1])
+    base4 = Polinomio([0, 0, 0, 1])
 
+    ortonormal = Polinomio.ortogonalizar([base1, base2, base3, base4])
 
-
-
-
-
+    print(ortonormal)
 
 
 main()
